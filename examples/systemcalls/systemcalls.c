52bf4e2fd6782a693e531dbb768adb23a7977f1c
vinnyaa <stokar2014@gmail.com>
2024-03-04 05:18:52 -0600

    adding 3p1 to the repo

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,11 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "fcntl.h"
+
 
 /**
  * @param cmd the command to execute with system()
@@ -17,7 +24,7 @@ bool do_system(const char *cmd)
  *   or false() if it returned a failure
 */
 
-    return true;
+    return system(cmd)!=-1;
 }
 
 /**
@@ -59,9 +66,42 @@ bool do_exec(int count, ...)
  *
 */
 
-    va_end(args);
+    //clear stdout buffer to avoid double printf statment
+    fflush(stdout);
+    int status;
+    pid_t p;
+    p = fork();
+    if(p==-1) {
+        // forking has failed, exit program
+        return false;
+    } else if(p==0) {
+        //the child process will do the work
+        execv(command[0], command);
+        // execv will never return here if working correctly. 
+        // return false to signify an error
+        exit(EXIT_FAILURE);
+        return false;
+    } else {
+        //parent process operations
 
-    return true;
+        // wait for child to return, verify no bad return value.
+        if(waitpid(p, &status,0)==-1) {
+            return false;
+        }
+        // verify no unexpected exit conditions for the child process
+        if(WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            // no unexpected condition, child completed correctly
+            va_end(args);
+            return true;
+        } else {
+            // error in child processing, return a failure
+            return false;
+        }
+    }
+
+    //va_end(args);
+    // should be unreachable, left for additional case coverage
+    return false;
 }
 
 /**
@@ -92,8 +132,53 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    
 
-    va_end(args);
 
-    return true;
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if(fd < 0){
+        printf("Error in opening of file\n");
+        exit(-1);
+    }
+
+    //clear stdout buffer to avoid double printf statment
+    fflush(stdout);
+    int status;
+    pid_t p;
+    p = fork();
+    if(p<0)
+    {
+        // forking has failed, exit program
+        exit(1);
+    } else if(p==0) {
+
+        if(dup2(fd,1)<0)
+        abort();
+        close(fd);
+        //the child process will do the work
+        execv(command[0], command);
+        // this return should never be reached, something failed
+        exit(EXIT_FAILURE);
+
+    } else {
+        //parent process operations
+
+        // wait for child to return, verify no bad return value.
+        if(waitpid(p, &status,0)==-1) {
+            return false;
+        }
+        // verify no unexpected exit conditions for the child process
+        if(WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            // no unexpected condition, child completed correctly
+            va_end(args);
+            return true;
+        } else {
+            // error in child processing, return a failure
+            return false;
+        }
+    }
+
+    //va_end(args);
+
+    return false;
 }
