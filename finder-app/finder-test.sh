6d5c255984119e503ac3673be7c9a1b8bfd15ed4
vinnyaa <stokar2014@gmail.com>
2024-04-26 10:49:47 -0500

    assignment 4 commit for finder test

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -10,6 +10,8 @@ WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat conf/username.txt)
 
+CONF_DIR=/etc/finder-app/conf
+
 if [ $# -lt 3 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
@@ -32,7 +34,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ${CONF_DIR}/assignment.txt`
 
 if [ $assignment != 'assignment3' ]
 then
